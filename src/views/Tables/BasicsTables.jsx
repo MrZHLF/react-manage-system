40a2f1cf49a7f48613581e1bfde146bdf00c4239
MrZHLF <480970024@qq.com>
2021-06-28 14:42:23 +0800

    ✨ feat: 表格

--- src/views/Tables/BasicsTables.jsx
+++ src/views/Tables/BasicsTables.jsx
@@ -1,31 +1,46 @@
 import React, { useState } from 'react'
-import { Table,Button,Row,Col,Modal,Form,Input,Space } from 'antd';
+import { Table,Button,Row,Col,Modal,Form,Input,Avatar  } from 'antd';
 function Tables () {
   
   const columns = [{
     title: '姓名',
     dataIndex: 'name',
     key: 'name',
+    align:"center"
   }, {
     title: '年龄',
     dataIndex: 'age',
     key: 'age',
+    align:"center"
   },{
     title: '性别',
     dataIndex: 'sex',
     key: 'sex',
+    align:"center",
     render:(text,rowData) => {
       return (
         <div>{rowData.sex == 0 ? '男' : '女'}</div>
       )
     }
   }, {
+    title: '头像',
+    dataIndex: 'avatar',
+    key: 'avatar',
+    align:"center",
+    render:(text,rowData) => {
+      return (
+        <Avatar src={rowData.avatar} size={40} alt="头像"/>
+      )
+    }
+  },{
     title: '地址',
     dataIndex: 'address',
     key: 'address',
+    align:"center"
     }, {
     title: "操作",
     key: "action",
+    align:"center",
     render: (text, rowData) => {
       return (
         <div>
@@ -41,6 +56,7 @@ function Tables () {
     id:1,
     key: '1',
     name: '张三',
+    avatar:"https://cmsres.dianzhenkeji.com/anonymous/2021/6/9/1402554386424664064.png",
     age: 20,
     sex: 0,
     address:"郑州市"
@@ -48,6 +64,7 @@ function Tables () {
     id:2,
     key: '2',
     name: '李四',
+    avatar:"https://cmsres.dianzhenkeji.com/anonymous/2021/4/1/1377440035200176128.jpg",
     age: 25,
     sex: 1,
     address:"周口市"
